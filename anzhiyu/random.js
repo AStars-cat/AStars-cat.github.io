e805cadb804e544879b5f027593c1f438a726374
realbyh <realbyh@qq.com>
2023-10-21 22:56:57 +0800

    Site updated: 2023-10-21 22:56:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/16/Cpp-I/","2023/10/16/Cpp-Ⅱ/","2023/10/16/life/2023-10-16-record/","2023/10/16/tt/","2023/10/20/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/16/Cpp-I/","2023/10/16/Cpp-Ⅱ/","2023/10/16/life/2023-10-16-record/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};

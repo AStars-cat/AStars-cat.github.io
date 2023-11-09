ff66f4c51253ade74f58999fc5220f0fd076f74e
realbyh <realbyh@qq.com>
2023-11-09 14:26:59 +0800

    Site updated: 2023-11-09 14:26:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/16/Cpp-I/","2023/10/16/Cpp-Ⅱ/","2023/10/16/life/2023-10-16-record/","2023/11/02/大学生活/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/16/life/2023-10-16-record/","2023/11/02/大学生活/","2023/10/16/CPP/Cpp-I/","2023/10/16/CPP/Cpp-Ⅱ/","2023/11/09/CPP/stdvector使用优化/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
